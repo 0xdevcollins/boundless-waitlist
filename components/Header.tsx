5a0d96e020f522bb5ada0db370ceeba5c4001448
Benjtalkshow <chinedubenj@gmail.com>
2024-12-16 17:19:28 +0100

    add logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link'
 import { Button } from "@/components/ui/button"
+import Image from 'next/image'
 
 export default function Header() {
   return (
     <header className="py-6 px-4 md:px-6 lg:px-8">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-2xl font-bold text-blue-600">Boundless</Link>
+        <Link href="/" className="text-2xl font-bold text-blue-600"><Image alt='boundless-logo' width={200} height={200} src={`/boundless-svg-logo.svg`} /></Link>
         <nav className="flex gap-4">
           <Button variant="ghost" asChild>
             <Link href="#about">About Us</Link>
